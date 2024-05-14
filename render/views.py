dcbc845bf3b8c93adecdeb4c0da67510b09ab5fc
TanakaKazushiro <tnk30125146@gmail.com>
2024-05-14 12:29:14 +0900

    トップページ作成

--- render/views.py
+++ render/views.py
@@ -5,5 +5,5 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 def index(request):
-    # user = User.objects.get(username="aaaaaa")
+    user = User.objects.get(username="aaaaaa")
     return render(request, "render/index.html", {"user":user})
