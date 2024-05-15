4e31bd4810c2cfdf996dd2a18db48a3c838264c8
tnkkzsr <tnk30125146@gmail.com>
2024-05-15 13:35:40 +0900

    種類ごとにページ分け

--- render/urls.py
+++ render/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("asahiyaki", views.asahiyaki, name="asahiyaki"),
+    path("mokkogei", views.mokkogei, name="mokkogei"),
 ]
--- render/views.py
+++ render/views.py
@@ -5,12 +5,24 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 def index(request):
+    
+    return render(request, "render/index.html")
+
+
+def asahiyaki(request):
     user = User.objects.get(username="aaaaaa")
     numbers = list(range(1,25))
     context = {
         "user": user,
         "numbers": numbers,
     }
-    return render(request, "render/index.html", context)
+    return render(request, "render/asahiyaki.html", context)
 
-
+def mokkogei(request):
+    user = User.objects.get(username="aaaaaa")
+    numbers = list(range(1,25))
+    context = {
+        "user": user,
+        "numbers": numbers,
+    }
+    return render(request, "render/mokkogei.html", context)
