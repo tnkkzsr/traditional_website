8b9e6bbf3e20e254f32a85dac7fef74c732e076e
tnkkzsr <tnk30125146@gmail.com>
2024-05-15 11:49:49 +0900

    赤線同期

--- render/static/js/index.js
+++ render/static/js/index.js
@@ -20,15 +20,51 @@ document.addEventListener("DOMContentLoaded", function() {
         },
     });
 
-    // スライド画像のクリックイベント
+    //画像一覧、スライドのクリックイベント
+    const ImageItems = document.querySelectorAll(".image-item");
+    let selectedItem = null;
     const slideImages = document.querySelectorAll(".swiper-slide");
     let selectedSlide = null;
 
+    ImageItems.forEach((item) => {
+        item.addEventListener("click", function() {
+            const selectedImage = item.getAttribute("data-image-item");
+            // 選択された画像をフォームにセット
+            document.getElementById("selected_image").value = selectedImage;
+            
+
+            // 以前選択された画像から選択クラスを削除
+            if (selectedItem) {
+                selectedItem.classList.remove("selected-image");
+            }
+
+            // 現在の画像に選択クラスを追加
+            item.classList.add("selected-image");
+
+            // 現在の画像を記録
+            selectedItem = item;
+
+            // スライドの赤枠も選択画像に合わせて移動
+            slideImages.forEach((image,index) => {
+                image.classList.remove("selected-image");
+                if (image.getAttribute("data-image") === selectedImage) {
+                    image.classList.add("selected-image");
+                    selectedSlide = image;
+                    swiper.slideTo(index-1);
+                }
+            })
+            
+        });
+    });
+
+
+    // スライド画像のクリックイベント
     slideImages.forEach((image) => {
         image.addEventListener("click", function() {
+            // 選択されたスライドをフォームにセット
             const selectedImage = image.getAttribute("data-image");
             document.getElementById("selected_image").value = selectedImage;
-            console.log(selectedImage);
+            
 
             // 以前選択されたスライドから選択クラスを削除
             if (selectedSlide) {
@@ -40,6 +76,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
             // 現在のスライドを記録
             selectedSlide = image;
+
+            // スライドの赤枠も選択画像に合わせて移動
+            ImageItems.forEach((image) => {
+                image.classList.remove("selected-image");
+                if (image.getAttribute("data-image-item") === selectedImage) {
+                    image.classList.add("selected-image");
+                    selectedItem = image;
+                }
+            });
         });
     });
 
