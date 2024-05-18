bf31639863ed2f8b8e5126877e03c79b5c559f1d
tnkkzsr <tnk30125146@gmail.com>
2024-05-18 17:35:06 +0900

    デプロイ用

--- config/settings.py
+++ config/settings.py
@@ -8,8 +8,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SECRET_KEY", default="input your dev mode secret key here")
 
-# DEBUG = "RENDER" not in os.environ
-DEBUG = True
+DEBUG = "RENDER" not in os.environ
+# DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0']
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
