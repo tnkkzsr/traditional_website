3d02fa12993cacb70f6067efe7cfad983939db58
tnkkzsr <tnk30125146@gmail.com>
2024-05-21 21:28:59 +0900

    設定ファイル変更

--- config/settings.py
+++ config/settings.py
@@ -149,38 +149,3 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 # }
 
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
-
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['console'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-    },
-}
