7e6854420ce8b87913b4c135fe94d764ad2183c9
tnkkzsr <tnk30125146@gmail.com>
2024-05-21 20:58:43 +0900

    uuidによるユーザー認証

--- render/views.py
+++ render/views.py
@@ -1,18 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect ,get_object_or_404
 from django.contrib.auth import get_user_model
+from .forms import UUIDForm
+from .models import User
 
 
 
 
 def index(request):
-    
-    return render(request, "render/index.html")
+    if request.method == "POST":
+        form = UUIDForm(request.POST)
+        if form.is_valid():
+            uuid = form.cleaned_data["uuid"]
+            return redirect("asahiyaki",uuid)
+    else:
+        form = UUIDForm()
+        
+    context = {
+        "form": form,
+        
+    }
+    return render(request, "render/index.html",context)
 
 
-def asahiyaki(request):
+def asahiyaki(request,uuid):
+    user = get_object_or_404(User, uuid=uuid)   
     numbers = list(range(1,25))
     context = {
         "numbers": numbers,
+        "user": user,
     }
     return render(request, "render/asahiyaki.html", context)
 
