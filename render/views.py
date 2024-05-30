f701c39b7ff937f9b18e50b737b861699bccc28c
tnkkzsr <tnk30125146@gmail.com>
2024-05-30 15:52:51 +0900

    非同期で保存

--- render/views.py
+++ render/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect ,get_object_or_404
 from django.contrib.auth import get_user_model
 from .forms import UUIDForm
-from .models import User,Asahiyaki
-
-
+from .models import User,Asahiyaki,AsahiyakiEvaluation
+import json
+from django.http import JsonResponse
 
 
 def index(request):
@@ -23,18 +23,38 @@ def index(request):
 
 
 def asahiyaki(request):
+    
     uuid = request.GET.get("uuid")
-    asahiyakis = Asahiyaki.objects.all()
-    for asahiyaki in asahiyakis:
-        print(asahiyaki.image_path)
+    
     if not uuid:
         return redirect("/")
-    user = User.objects.get_or_create(uuid=uuid, defaults={"username": f'User_{uuid}'})   
+    
+    asahiyakis = Asahiyaki.objects.all()
+    # ユーザーが存在しない場合は新規作成
+    if not User.objects.filter(uuid=uuid).exists():
+        User.objects.create(uuid=uuid)
+    user = User.objects.get(uuid=uuid)   
+    
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        asahiyaki = Asahiyaki.objects.get(id=data['asahiyaki'])
+        selected_image = data['selected_image']        
+        evaluation = data['evaluation']
+        akashiyaki_evaluation = AsahiyakiEvaluation.objects.filter(user=user, asahiyaki=asahiyaki)
+        # すでに評価が存在する場合は更新、存在しない場合は新規作成
+        if akashiyaki_evaluation.exists():
+            akashiyaki_evaluation.update(front_image_name=selected_image, evaluation=evaluation)
+        else:
+            evaluation = AsahiyakiEvaluation.objects.create(user=user, asahiyaki=asahiyaki, front_image_name=selected_image, evaluation="A")
+        return JsonResponse({'status': 'success', 'message': 'データが正常に保存されました。'})
+
+    
+    
     numbers = list(range(1,25))
     context = {
         "numbers": numbers,
-        "user": user,
         "asahiyakis": asahiyakis,
+        "user": user,
     }
     return render(request, "render/asahiyaki.html", context)
 
