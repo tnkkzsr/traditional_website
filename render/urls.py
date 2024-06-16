32434149b124f38dea2815a35f0b720405448374
tnkkzsr <tnk30125146@gmail.com>
2024-06-17 08:11:15 +0900

    結果ページ作成

--- render/urls.py
+++ render/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("asahiyaki/", views.asahiyaki, name="asahiyaki"),
     path("asahiyaki/learn/", views.asahiyaki_learn, name="asahiyaki_learn"),
+    path('evaluation_results/<uuid:user_uuid>/', views.evaluation_results, name='evaluation_results'),
+
     path("mokkogei", views.mokkogei, name="mokkogei"),
 ]
