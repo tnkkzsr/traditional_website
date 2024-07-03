8b5c351a18dd58416205e522e9ec5bfd6337dba0
tnkkzsr <tnk30125146@gmail.com>
2024-07-03 22:34:00 +0900

    木工芸のurl作成

--- render/urls.py
+++ render/urls.py
@@ -3,12 +3,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
     path("", views.index, name="index"),
+    path("login", views.login, name="login"),
     path("asahiyaki/", views.asahiyaki, name="asahiyaki"),
-    path("asahiyaki/learn/", views.asahiyaki_learn, name="asahiyaki_learn"),
+    path("asahiyaki/learn", views.asahiyaki_learn, name="asahiyaki_learn"),
     path('evaluation_results/<uuid:user_uuid>/', views.evaluation_results, name='evaluation_results'),
     path('asahiyaki/select_front/', views.asahiyaki_select_front, name='select_front'),
     path('asahiyaki/select_front/learn', views.asahiyaki_front_select_learn, name='front_select_learn'),
     path("mokkogei", views.mokkogei, name="mokkogei"),
     path("mokkogei/learn", views.mokkogei_learn, name="mokkogei_learn"),
+    path("mokkogei/result", views.mokkogei_result, name="mokkogei_result"),
 ]
